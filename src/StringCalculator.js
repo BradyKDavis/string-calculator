9733ee92430f19ef8d7da5c1fc765eac1f22da2e
Brady Davis <bradydavis.ver.2.0@gmail.com>
2018-02-08 12:02:09 -0600

    Adding initial tests and solutions
    should have been doing this on a test by case basis :/

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,4 +1,11 @@
+import IntegerParser from './IntegerParser'
+
 export default class StringCalculator {
-  static add() {
+  static add(args) {
+      var sum = 0;
+      for (var i = 0; i < arguments.length; i++) {
+        sum += IntegerParser.parse(arguments[i]);
+    }
+      return sum;
   }
 }
--- test/StringCalculator.spec.js
+++ test/StringCalculator.spec.js
@@ -1,8 +1,23 @@
 import {expect} from 'chai';
 import StringCalculator from '../src/StringCalculator';
 
+
 describe('StringCalculator', () => {
   it('should do something...', () => {
     expect('testing driven development').to.include('drive');
   });
 });
+
+describe('StringCalculator add', () =>{
+    it('should return 0 when given no arguments', () => {
+        expect(StringCalculator.add()).to.equal(0);
+    });
+
+    it('should return number when string number argument passed in', () => {
+        expect(StringCalculator.add('7')).to.equal(7);
+    });
+
+    it('should return sum when two string number arguments are passed in', () =>{
+        expect(StringCalculator.add('55', '11')).to.equal(66);
+    });
+})
