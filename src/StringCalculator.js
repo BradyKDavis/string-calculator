157c7722a39aecd2f9c5e522580cce023c8e5d1f
Brady Davis <bradydavis.ver.2.0@gmail.com>
2018-02-08 12:08:49 -0600

    String Calculator now ignores null arguments

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -4,7 +4,10 @@ export default class StringCalculator {
   static add(args) {
       var sum = 0;
       for (var i = 0; i < arguments.length; i++) {
-        sum += IntegerParser.parse(arguments[i]);
+          if(arguments[i] !== null)
+          {
+              sum += IntegerParser.parse(arguments[i]);
+          }
     }
       return sum;
   }
